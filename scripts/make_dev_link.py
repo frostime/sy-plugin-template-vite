8b81cc50895cfea4a8c47fbd49ce1f1f5b19c381
frostime <frostime@foxmail.com>
2023-05-18 19:47:41 +0800

    自动创建 dev 目录

--- scripts/make_dev_link.py
+++ scripts/make_dev_link.py
@@ -17,12 +17,16 @@ name = content.get('name')
 if not name or name == '':
     print('"name" in plugin.json not found, exit')
     exit()
+    
+dev_dir = os.path.abspath('dev')
+if not os.path.exists(dev_dir):
+    os.mkdir(dev_dir)
 
 # 3. 读取 plugin_dir 下的是否有和 name 字段相同的文件夹
 # ...如果没有，创建一个软链接到当前根目录下的 dev 文件夹
 # ...如果有，报错并退出
 if not os.path.exists(os.path.join(plugin_dir, name)):
-    os.symlink(os.path.abspath('dev'), os.path.join(plugin_dir, name))
+    os.symlink(dev_dir, os.path.join(plugin_dir, name))
 else:
     print('Folder already exists, exit')
     exit()
