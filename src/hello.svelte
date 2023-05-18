69d469f4d2d93c56c386306b56aded5c7d0e8f30
frostime <frostime@foxmail.com>
2023-05-18 13:45:52 +0800

    似乎基本可用，还需要打磨一下

--- src/hello.svelte
+++ src/hello.svelte
@@ -2,6 +2,13 @@
     export let name: string;
 </script>
 
-<div>
+<div id="hello">
     Hello {name}!
 </div>
+
+<style>
+    #hello {
+        margin: 2em;
+        text-align: center;
+    }
+</style>
